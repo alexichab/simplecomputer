1c66774f55cbc0b9ecf4890862a55e7f5bab358f
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-03-11 02:24:40 +0700

    Add function and connceted term_gui.h library

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "prototype.h"
 #include "term.h"
+#include "term_gui.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,5 +8,17 @@
 int
 main ()
 {
+  mt_clrscr ();
+  sc_memoryInit ();
+  for (int i = 3; i < 100; i *= 3)
+    {
+      sc_memorySet (i, i);
+    }
+  sc_regInit ();
+  sc_regSet (1, 1);
+  sc_regSet (2, 1);
+  sc_regSet (4, 1);
+  sc_regSet (16, 1);
+  out_GUI ();
   return 0;
 }
