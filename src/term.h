9857847662b86ab5732c021272f160d518bc8da6
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-03-11 02:08:59 +0700

    Add functions in term.h

--- src/term.h
+++ src/term.h
@@ -1,1 +1,35 @@
+#pragma once
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#define CLEAR "\E[H\E[2J"
+
+enum colors
+{
+  BLACK,
+  RED,
+  GREEN,
+  YELLOW,
+  BLUE,
+  MAGENTA,
+  CYAN,
+  WHITE,
+  UNKNOWN,
+  DEFAULT
+};
+
+int mt_clrscr (void); // очищает экран и перемещает курсор в верхний левый угол
+int mt_gotoXY (
+    int x,
+    int y); // перемещает курсор к введенным координатам (x, y) = (row, col)
+int mt_getscreensize (
+    int *rows, int *cols); //получает размер экрана терминала (количество строк и столбцов)
+int mt_setfgcolor (enum colors color); // устанавливает цвет фона для всех и столбцов всего терминала
+int mt_setbgcolor (
+    enum colors color); // устанавливает цвет фона только для предстоящих символов
 
