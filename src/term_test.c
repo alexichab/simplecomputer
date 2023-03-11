ab4cd34b79548c54f881a7674ead087363e894ee
Alexey Naumov <alexeynaumov@Lenovo-Legion-5-15ARH05H-267a6435.localdomain>
2023-03-11 17:05:25 +0700

    Fixed test

--- src/term_test.c
+++ src/term_test.c
@@ -5,20 +5,21 @@ int
 term_test ()
 {
   mt_clrscr ();
-  printf ("Привет, это терминал по умолчанию\n");
+  printf ("Терминал по умолчанию\n");
   getchar ();
 
   mt_clrscr ();
+  mt_setbgcolor (BLUE);
+  printf (" Синий фон для вывода текста\n");
+  getchar ();
+
   mt_setbgcolor (GREEN);
-  printf ("Привет, это зеленый фон для вывода текста\n");
-  getchar ();
-
-  mt_setbgcolor (DEFAULT);
   mt_clrscr ();
-  mt_setfgcolor (RED);
-  printf ("Привет, это красный фон для всего\n");
+  mt_setfgcolor (BLACK);
+  printf ("Зеленый фон для всего\n");
   getchar ();
 
   mt_clrscr ();
+  printf("\E[H\E[2J\033[0m\033[39m");
   return 0;
 }
